e0916eb59b347377adc0658be9bdbb4dedb81cdb
felipekuang <126790396+felipekuang@users.noreply.github.com>
2023-08-29 21:32:33 -0300

    Update myscrypt.js

--- docs/myscrypt.js
+++ docs/myscrypt.js
@@ -1,3 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const animatedElement = document.getElementById("animatedElement");
+    animatedElement.classList.add("show");
+});
+const indexLinks = document.querySelectorAll(".index a");
+
+indexLinks.forEach(link => {
+    link.addEventListener("click", smoothScroll);
+});
+
+function smoothScroll(event) {
+    event.preventDefault();
+    
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    const offset = targetElement.getBoundingClientRect().top;
+    
+    window.scrollTo({
+        top: offset + window.scrollY,
+        behavior: "smooth"
+    });
+}
 // function scrollToPage(pageId) {
 //     var page = document.getElementById(pageId);
 //     page.scrollIntoView({
